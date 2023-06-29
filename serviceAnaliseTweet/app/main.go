9fc95ba3d9abfcad2596b6ffb4ca203d650268a3
GaberRB <grios0311@gmail.com>
2023-06-28 21:51:36 -0300

    feat: servicoAnaliseSentimento golang

--- serviceAnaliseTweet/app/main.go
+++ serviceAnaliseTweet/app/main.go
@@ -1,5 +1,20 @@
 package main
-import "fmt"
+
+import (
+	"app/kafka"
+	"fmt"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
 func main() {
-    fmt.Println("hello world")
+	fmt.Print("Iniciando o servidor...ss")
+	// metric.GetMetric()
+	// http.ListenAndServe(":8080", nil)
+
+	http.Handle("/metrics", promhttp.Handler())
+	go kafka.Listen()
+	http.ListenAndServe(":8080", nil)
+
 }
