9b8509b14ac143c7ff7c212e7de16b1b263c1a64
GaberRB <grios0311@gmail.com>
2023-07-13 18:52:13 -0300

    feat: producer python

--- docker/init.sql
+++ docker/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE analises_sentimentos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   sentiment VARCHAR(10),
-  tweet VARCHAR(250),
+  tweet TEXT,
   mixed DOUBLE,
   negative DOUBLE,
   neutral DOUBLE,
